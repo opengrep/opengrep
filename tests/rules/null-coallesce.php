dc32149c8a35b96cac258172ca30ed87af735465
Dimitris Mostrous <dimitris.mostrous+aikido@gmail.com>
2025-06-13 17:25:40 +0100

    fix tainting test
    But it fails

--- tests/rules/null-coallesce.php
+++ tests/rules/null-coallesce.php
@@ -5,7 +5,8 @@ function f($filename) {
     "convert $filename";
 }
 function f1($filename) {
+    // ruleid: test
     return shell_exec($filename)
-            ?? 'Failed to get filename';
+            ?? throw new RuntimeException('Failed to get filename');
 }
 
