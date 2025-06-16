4381d82ad3c6a6716e1bc13c2c16bc5dfe41e05a
Corneliu Hoffman <corneliuhoffman@Cornelius-MacBook-Pro.local>
2025-06-16 14:17:29 +0100

    failing test fro match

---
+++ tests/rules/match-rules.php
@@ -0,0 +1,13 @@
+<?php
+
+function f($filename) {
+    // ruleid: test
+    bad("convert $filename");
+}
+function f2($filename) {
+    // ruleid: test
+    return match($filename) {
+              1 => 'one',
+              2 => 'two',
+};
+}
