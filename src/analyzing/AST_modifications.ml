15c946bb73a7cade4e8006ba1404d81d82b07990
Corneliu Hoffman <corneliuhoffman@Cornelius-MacBook-Pro.local>
2025-12-05 16:36:20 +0000

    extended Elixir parsing fix

--- src/analyzing/AST_modifications.ml
+++ src/analyzing/AST_modifications.ml
@@ -73,73 +73,22 @@ let convert_elixir_implicit_lambda (fdef : G.function_definition)
 (* Elixir: ShortLambda / Capture operator *)
 (* ========================================================================== *)
 
-(** Elixir: Find the maximum placeholder number in a ShortLambda body.
-    E.g., in &(foo(&1, &3)), returns 3. *)
-let rec find_max_placeholder (e : G.expr) : int =
-  match e.G.e with
-  | G.OtherExpr
-      (("PlaceHolder", _), [ G.E { e = G.L (G.Int (Some n, _)); _ } ]) ->
-      Int64.to_int n
-  | G.Call (callee, (_, args, _)) ->
-      let callee_max = find_max_placeholder callee in
-      let args_max =
-        args
-        |> List.fold_left
-             (fun acc arg ->
-               match arg with
-               | G.Arg e | G.ArgKwd (_, e) -> max acc (find_max_placeholder e)
-               | _ -> acc)
-             0
-      in
-      max callee_max args_max
-  | _ -> 0
+(** Elixir: Convert ShortLambda to a regular Lambda.
 
-(** Elixir: Replace placeholder references with parameter names.
-    E.g., &1 becomes x0, &2 becomes x1, etc. *)
-let rec replace_placeholders (e : G.expr) : G.expr =
+    ShortLambda comes from Elixir_to_generic as:
+    OtherExpr("ShortLambda", [Params params; S body_stmt])
+
+    This converts it to a proper Lambda for IL/taint analysis. *)
+let convert_elixir_short_lambda (e : G.expr) : G.expr =
   match e.G.e with
-  | G.OtherExpr (("PlaceHolder", _), [ G.E { e = G.L (G.Int (Some n, tk)); _ } ])
-    ->
-      let param_idx = Int64.to_int n - 1 in
-      let param_name = Printf.sprintf "x%d" param_idx in
-      let param_id = (param_name, tk) in
-      G.N (G.Id (param_id, G.empty_id_info ())) |> G.e
-  | G.Call (callee, (lp, args, rp)) ->
-      let new_callee = replace_placeholders callee in
-      let new_args =
-        args
-        |> List.map (fun arg ->
-               match arg with
-               | G.Arg e -> G.Arg (replace_placeholders e)
-               | G.ArgKwd (id, e) -> G.ArgKwd (id, replace_placeholders e)
-               | other -> other)
+  | G.OtherExpr (("ShortLambda", tok), [ G.Params params; G.S body ]) ->
+      let fdef =
+        {
+          G.fparams = Tok.unsafe_fake_bracket params;
+          frettype = None;
+          fkind = (G.LambdaKind, tok);
+          fbody = G.FBStmt body;
+        }
       in
-      G.Call (new_callee, (lp, new_args, rp)) |> G.e
+      G.Lambda fdef |> G.e
   | _ -> e
-
-(** Elixir: Convert ShortLambda/Capture (&expression) to a regular Lambda.
-
-    Elixir's capture operator & creates anonymous functions:
-    - &foo/1 captures a function reference (converted at parse time)
-    - &(sink(&1)) creates a lambda where &1 is the first parameter
-
-    This converts captures with placeholders to regular lambdas for taint analysis. *)
-let convert_elixir_short_lambda (tok : Tok.t) (body_expr : G.expr) : G.expr =
-  let max_placeholder = find_max_placeholder body_expr in
-  let replaced_body = replace_placeholders body_expr in
-  let params =
-    List.init max_placeholder (fun i ->
-        let param_name = Printf.sprintf "x%d" i in
-        let param_id = (param_name, tok) in
-        G.Param (G.param_of_id param_id))
-  in
-  let body_stmt = G.ExprStmt (replaced_body, G.sc) |> G.s in
-  let fdef =
-    {
-      G.fparams = Tok.unsafe_fake_bracket params;
-      frettype = None;
-      fkind = (G.LambdaKind, tok);
-      fbody = G.FBStmt body_stmt;
-    }
-  in
-  G.Lambda fdef |> G.e
