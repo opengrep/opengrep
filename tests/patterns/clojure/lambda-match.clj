e194e81ba4d7c3fcf11b5f9e4c1fb78c83015fb3
Dimitris Mostrous <dimitris.mostrous+aikido@gmail.com>
2026-01-02 16:58:44 +0000

    improve comments and add some more todos

--- tests/patterns/clojure/lambda-match.clj
+++ tests/patterns/clojure/lambda-match.clj
@@ -11,3 +11,7 @@
 ;;
 ;; Note: the inverse does not match:
 ;; a pattern (-> [...] (fn ...)) only matches -> forms.
+;;
+;; TODO: If we really want this to be compositional, we need to
+;; allow E to match with OtherExpr(_, [ G.E E ]) in clojure.
+;; But this should propably not be the general case.
