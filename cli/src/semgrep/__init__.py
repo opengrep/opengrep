837ebcc3c4032a26455dc8e31e509a19eb5dbdbe
Maciej Piróg <maciej.adam.pirog@gmail.com>
2026-09-05 00:48:13 +0200

    bump version

--- cli/src/semgrep/__init__.py
+++ cli/src/semgrep/__init__.py
@@ -1,2 +1,2 @@
-__VERSION__ = "1.29.0"
+__VERSION__ = "1.30.0"
 __SEMGREP_VERSION__ = "1.100.0"
--- src/core/Version.ml
+++ src/core/Version.ml
@@ -3,5 +3,5 @@
 
   Automatically modified by scripts/release/bump.
 *)
-let version = "1.29.0"
+let version = "1.30.0"
 let version_semgrep = "1.100.0"
